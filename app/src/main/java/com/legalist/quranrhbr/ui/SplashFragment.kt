d86f71e67d1a8b79ef41485745ccfdb73c528145
Ayaz Mammadov <ayazmmmm.1990@gmail.com>
2024-05-15 02:07:42 +0400

    splash screen created

--- app/src/main/java/com/legalist/quranrhbr/ui/SplashFragment.kt
+++ app/src/main/java/com/legalist/quranrhbr/ui/SplashFragment.kt
@@ -1,12 +1,16 @@
 package com.legalist.quranrhbr.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.legalist.quranrhbr.R
 import com.legalist.quranrhbr.databinding.FragmentSplashBinding
+import com.legalist.quranrhbr.viewModel.SplashScreenViewModel
 
 class SplashFragment : Fragment() {
     private lateinit var  binding:FragmentSplashBinding
@@ -16,6 +20,8 @@ class SplashFragment : Fragment() {
 
     }
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +30,13 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
      binding = FragmentSplashBinding.inflate(inflater, container, false)
         return  binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val viewModel = ViewModelProvider(requireActivity()).get(SplashScreenViewModel::class.java)
+        viewModel.navigateToNextFragment(findNavController())
+
     }
 
 }
