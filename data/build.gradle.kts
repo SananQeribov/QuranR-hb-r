bb0bb80d4941b6e84c797dae0871fd914c1f6d55
Ayaz Mammadov <ayazmmmm.1990@gmail.com>
2024-06-05 17:09:07 +0400

    Quran api changed

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -109,4 +109,8 @@ dependencies {
     api ( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
+
+    implementation ("com.squareup.moshi:moshi:1.12.0")
+    implementation ("com.squareup.moshi:moshi-kotlin:1.12.0")
+
 }
