099db619102f4e93169cdc15c38e7c74aad78686
QaribovSenan <qeribovsenan873@gmail.com>
2024-06-05 07:30:43 -0700

    added anim

--- app/src/main/java/com/legalist/quranrhbr/adapter/SurahAdapter.kt
+++ app/src/main/java/com/legalist/quranrhbr/adapter/SurahAdapter.kt
@@ -1,21 +1,29 @@
 package com.legalist.quranrhbr.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.ders.domain.model.Surah
 import com.legalist.quranrhbr.R
-import android.widget.Filter
-import android.widget.Filterable
-import java.util.*
+import java.util.Locale
 
-class SurahAdapter(private val surahList: List<Surah>,
-                   private val onItemClick: (position: Int) -> Unit) :
+
+class SurahAdapter(
+    private val surahList: List<Surah>,
+    private val onItemClick: (position: Int) -> Unit
+) :
     RecyclerView.Adapter<SurahAdapter.SurahViewHolder>(), Filterable {
 
     private var filteredSurahList: List<Surah> = surahList
+    private var lastPosition = -1
 
     inner class SurahViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val number: TextView = view.findViewById(R.id.surah_number)
@@ -24,6 +32,7 @@ class SurahAdapter(private val surahList: List<Surah>,
         val translation: TextView = view.findViewById(R.id.surah_translation)
         val ayahsNumber: TextView = view.findViewById(R.id.ayahs_number)
         val revelationType: TextView = view.findViewById(R.id.revelationType)
+        val cardview: CardView = view.findViewById(R.id.card)
 
         init {
             view.setOnClickListener {
@@ -37,7 +46,10 @@ class SurahAdapter(private val surahList: List<Surah>,
         return SurahViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: SurahViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: SurahViewHolder,
+        @SuppressLint("RecyclerView") position: Int
+    ) {
         val surah = filteredSurahList[position]
         holder.number.text = (position + 1).toString()
         holder.name.text = surah.name
@@ -45,6 +57,23 @@ class SurahAdapter(private val surahList: List<Surah>,
         holder.translation.text = surah.englishNameTranslation
         holder.ayahsNumber.text = surah.numberOfAyahs.toString()
         holder.revelationType.text = surah.revelationType
+        if (position > lastPosition) {
+            //TranslateAnimation anim = new TranslateAnimation(0,-1000,0,-1000);
+            val anim = ScaleAnimation(
+                0.0f,
+                1.0f,
+                0.0f,
+                1.0f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            //anim.setDuration(new Random().nextInt(501));//to make duration random number between [0,501)
+            anim.duration = 550 //to make duration random number between [0,501)
+            holder.cardview.startAnimation(anim)
+            lastPosition = position
+        }
         holder.itemView.setOnClickListener {
             onItemClick(position)
         }
@@ -62,11 +91,15 @@ class SurahAdapter(private val surahList: List<Surah>,
                 if (constraint.isNullOrBlank()) {
                     filteredResults.addAll(surahList)
                 } else {
-                    val filterPattern = constraint.toString().trim().toLowerCase(Locale.getDefault())
+                    val filterPattern =
+                        constraint.toString().trim().toLowerCase(Locale.getDefault())
                     for (item in surahList) {
                         if (item.name.toLowerCase(Locale.getDefault()).contains(filterPattern) ||
-                            item.englishName.toLowerCase(Locale.getDefault()).contains(filterPattern) ||
-                            item.englishNameTranslation.toLowerCase(Locale.getDefault()).contains(filterPattern)) {
+                            item.englishName.toLowerCase(Locale.getDefault())
+                                .contains(filterPattern) ||
+                            item.englishNameTranslation.toLowerCase(Locale.getDefault())
+                                .contains(filterPattern)
+                        ) {
                             filteredResults.add(item)
                         }
                     }
