566874bfab34f17e5aea5a083b7a628ddbb58753
Ayaz Mammadov <ayazmmmm.1990@gmail.com>
2024-06-11 19:15:33 +0400

    new codes added

--- app/src/main/java/com/legalist/quranrhbr/ui/SplashdefaultFragment.kt
+++ app/src/main/java/com/legalist/quranrhbr/ui/SplashdefaultFragment.kt
@@ -13,34 +13,56 @@ import androidx.navigation.fragment.findNavController
 
 
 
+//class SplashdefaultFragment : Fragment() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//
+//
+//        // 5 saniyə = 5,000 millisaniyə
+//        val delayInMillis: Long = 5_000
+//
+//        val handler = Handler(Looper.getMainLooper())
+//        handler.postDelayed({
+//            findNavController().navigate(R.id.action_splashdefaultFragment_to_splashFragment)
+//        }, delayInMillis)
+//
+//
+//
+//
+//
+//
+//
+//        return inflater.inflate(R.layout.fragment_splashdefault, container, false)
+//    }
+//
+//
+//}
 class SplashdefaultFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
-
-        // 5 saniyə = 5,000 millisaniyə
-        val delayInMillis: Long = 5_000
-
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            findNavController().navigate(R.id.action_splashdefaultFragment_to_splashFragment)
-        }, delayInMillis)
-
-
-
-
 
         return inflater.inflate(R.layout.fragment_splashdefault, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            findNavController().navigate(R.id.action_splashdefaultFragment_to_splashFragment)
+        }, 2000)
+    }
 }
